6fdace6a0e171c85a711ab26320a72ca26d999e4
Heather Seaman <heather@WL-209-4.WPA.Claremont.Edu>
2015-12-05 01:41:04 -0800

    randomize working

--- server.js
+++ server.js
@@ -11,10 +11,30 @@ app.use(express.static('public'));
 
 var port = process.env.PORT || 8000;
 
+var shuffle = function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex ;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+	// Pick a remaining element...
+	randomIndex = Math.floor(Math.random() * currentIndex);
+	currentIndex -= 1;
+
+	// And swap it with the current element.
+	temporaryValue = array[currentIndex];
+	array[currentIndex] = array[randomIndex];
+	array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+};
+
 app.get('/', function (req, res) {
 	getIds(function (imageIds) {
 		var imageCount = imageIds.length;
-		res.render('index', {title: 'Home', ids: imageIds});
+		var randomIds = shuffle(imageIds);
+		res.render('index', {title: 'Home', ids: randomIds});
 	});
 });
 
